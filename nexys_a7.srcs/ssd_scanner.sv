9e3087dede4eeb21f458577658ee3b860d3a7d94
John Yu <jdoe35087@gmail.com>
2021-02-06 16:05:25 -0500

    FEAT add ssd_scanner

---
+++ nexys_a7.srcs/ssd_scanner.sv
@@ -0,0 +1,41 @@
+`include "timescale.sv"
+module ssd_scanner #(
+	parameter integer unsigned DIG_COUNTER_WIDTH = 3,
+	parameter integer unsigned INTEN_COUNTER_WIDTH = 4,
+	parameter integer unsigned SEG_WIDTH = 8
+)
+(
+	input clock,
+	input reset,
+	input clock_en,
+	input [INTEN_COUNTER_WIDTH-1:0] inten,
+	input [SEG_WIDTH-1:0] seg [0:2**DIG_COUNTER_WIDTH-1],
+	output logic [2**DIG_COUNTER_WIDTH-1:0] dig_out,
+	output logic [SEG_WIDTH-1:0] seg_out
+);
+	logic [INTEN_COUNTER_WIDTH-1:0] inten_counter;
+	logic [DIG_COUNTER_WIDTH-1:0] dig_counter;
+	logic [INTEN_COUNTER_WIDTH-1:0] inten_d;
+
+	always_ff @(posedge clock) begin
+		priority if (reset) begin
+			dig_counter <= '0;
+			inten_counter <= '0;
+			dig_out <= '0;
+			seg_out <= '0;
+			inten_d <= '0;
+
+		end else if (clock_en) begin
+			inten_counter <= inten_counter + 1'b1;
+			dig_out <= '1;
+			dig_out[dig_counter] <= (inten_counter <= inten_d)? 1'b0 : 1'b1;
+			seg_out <= seg[dig_counter];
+			if (inten_counter == '1) begin
+				dig_counter <= dig_counter + 1'b1;
+				inten_d <= inten;
+			end
+		end else begin
+			// intentionally left blank
+		end
+	end
+endmodule
